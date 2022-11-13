b06eec5968843c051be2bcd797f8817c28021b81
Olian04 <rift95@live.se>
2022-11-13 20:06:50 +0100

    feat: Refactor types into composable traits

--- src/api.ts
+++ src/api.ts
@@ -6,3 +6,22 @@ export { createGroup } from './composites/createGroup';
 export { createResource } from './composites/createResource';
 
 export { visualizeDependencyGraph } from './utils/visualizeDependencyGraph';
+
+export { CleanupStrategy } from './enums/CleanupStrategy';
+
+export type { Atom } from './types/Atom';
+export type { Selector } from './types/Selector';
+export type { Resource } from './types/Resource';
+
+import type { Keyed as _Keyed } from './types/traits/Keyed';
+import type { Gettable as _Gettable } from './types/traits/Gettable';
+import type { Settable as _Settable } from './types/traits/Settable';
+import type { Subscribable as _Subscribable } from './types/traits/Subscribable';
+import type { Invalidatable as _Invalidatable } from './types/traits/Invalidatable';
+export namespace Traits {
+  export type Keyed = _Keyed;
+  export type Gettable<T> = _Gettable<T>;
+  export type Settable<T> = _Settable<T>;
+  export type Subscribable = _Subscribable;
+  export type Invalidatable = _Invalidatable;
+}
