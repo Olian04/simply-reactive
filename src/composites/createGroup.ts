67224d7c2595c260d0843e787e203fd66ef1b497
Olian04 <rift95@live.se>
2022-11-13 20:51:11 +0100

    feat: Implement createGroup

--- src/composites/createGroup.ts
+++ src/composites/createGroup.ts
@@ -1,3 +1,57 @@
-export const createGroup = () => {
-  throw new Error('Not yet implemented');
+import type { ImplementsGet } from '../types/traits/ImplementsGet';
+import type { ImplementsSubscribe } from '../types/traits/ImplementsSubscribe';
+import type { Group } from '../types/Group';
+
+import { getNextAutoKey } from '../globals';
+import { createAtom } from '../primitives/createAtom';
+import { createSelector } from '../api';
+
+export const createGroup = <
+  T,
+  Id extends string | number,
+  Value extends ImplementsSubscribe & ImplementsGet<T>
+>(props: {
+  key?: string;
+  getDefault: () => Value;
+}): Group<Id, Value> => {
+  const key = props.key || getNextAutoKey();
+
+  const Container = createAtom({
+    key: `${key}_group_container`,
+    default: {} as { [k in Id]: Value },
+  });
+
+  const SubscribeTarget = createSelector({
+    key: `${key}_group_subscribe_target`,
+    get: () => {
+      const arr = Container.get(); // Subscribe to the container
+      Object.values<Value>(arr).forEach((V) => {
+        V.get(); // Subscribe to each value in the container
+      });
+    },
+  });
+
+  return {
+    key,
+    subscribe: SubscribeTarget.subscribe,
+    get: () => Object.values(Container.get()),
+    find: (id) => {
+      if (!Container.get()[id]) {
+        Container.set((container) => {
+          container[id] = props.getDefault();
+          return container;
+        });
+      }
+      return Container.get()[id];
+    },
+    remove: (id) => {
+      if (Container.get()[id]) {
+        Container.set((container) => {
+          delete container[id];
+          return container;
+        });
+      }
+    },
+    clear: () => Container.set({} as ReturnType<typeof Container.get>),
+  };
 };
